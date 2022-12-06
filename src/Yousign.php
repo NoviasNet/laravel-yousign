7fe44be93fab034bc5319938fb63d0adc83196e7
Gabriele Pistoia <gabriele.pistoia@assiclick.com>
2022-12-06 09:05:35 +0100

    wip

--- src/Yousign.php
+++ src/Yousign.php
@@ -2,11 +2,11 @@
 
 namespace Assiclick\Yousign;
 
+use Exception;
 use Assiclick\Yousign\Http\Client;
 use Assiclick\Yousign\Resources\BaseResource;
-use Exception;
 
-class YouSing
+class Yousing
 {
     protected Client $client;
 
@@ -23,11 +23,11 @@ class YouSing
     /**
      * Return an instance of a Resource based on the method called.
      *
-     * @param  mixed  $args
+     * @param mixed $args
      */
     public function __call(string $name, $args): BaseResource
     {
-        $resource = 'Assiclick\\Yousign\\Resources\\'.ucfirst($name);
+        $resource = 'Assiclick\\Yousign\\Resources\\' . ucfirst($name);
 
         /* @var BaseResource */
         return new $resource($this->client, ...$args);
