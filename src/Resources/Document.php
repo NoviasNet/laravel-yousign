f220a32bc2687c0043a0f15d66665aaf36a61edc
Gabriele Pistoia <gabriele.pistoia@assiclick.com>
2022-12-07 11:17:19 +0100

    Add Document Resources

---
+++ src/Resources/Document.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Assiclick\Yousign\Resources;
+
+class Document
+{
+    protected string $path = 'documents';
+
+    /**
+     * Upload a document.
+     *
+     * @param  array $data
+     * @return array
+     */
+    public function upload(array $data): array
+    {
+        return $this->client->request(
+            'post',
+            $this->path,
+            $this->parseObjArray($data)
+        )['data'];
+    }
+
+    protected function parseObjArray(array $params): array
+    {
+        return $params;
+    }
+}
