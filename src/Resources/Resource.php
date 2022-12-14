62147a8e35e19418c7fb3fdda33986c24d57f3c1
Gabriele Pistoia <gabriele.pistoia@assiclick.com>
2022-12-14 09:00:56 +0100

    wip

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -9,7 +9,7 @@ abstract class Resource extends BaseResource
     /**
      * Initialize Resource.
      *
-     * @param  Client  $client
+     * @param Client $client
      */
     public function __construct(Client $client, protected string $id = '')
     {
@@ -31,14 +31,14 @@ abstract class Resource extends BaseResource
             'post',
             $this->path,
             $this->parseObjArray($data)
-        )['data'];
+        );
     }
 
     public function update(array $data): array
     {
         return $this->client->request(
             'patch',
-            $this->path.'/'.$this->id,
+            $this->path . '/' . $this->id,
             $this->parseObjArray($data)
         );
     }
@@ -47,7 +47,7 @@ abstract class Resource extends BaseResource
     {
         return $this->client->request(
             'delete',
-            $this->path.'/'.$this->id,
+            $this->path . '/' . $this->id,
             []
         );
     }
@@ -56,7 +56,7 @@ abstract class Resource extends BaseResource
     {
         return $this->client->request(
             'get',
-            $this->path.'/'.$this->id,
+            $this->path . '/' . $this->id,
             []
         );
     }
