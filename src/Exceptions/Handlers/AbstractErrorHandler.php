22264512d35d0d78bc3c515050004c31894f46ce
ToshiZL <ToshiZL@users.noreply.github.com>
2023-04-26 09:09:14 +0000

    Fix styling

--- src/Exceptions/Handlers/AbstractErrorHandler.php
+++ src/Exceptions/Handlers/AbstractErrorHandler.php
@@ -2,8 +2,8 @@
 
 namespace Assiclick\Yousign\Exceptions\Handlers;
 
-use Assiclick\Yousign\Http\Client;
 use Assiclick\Yousign\Exceptions\YousignException;
+use Assiclick\Yousign\Http\Client;
 
 /**
  * Class AbstractErrorHandler.
