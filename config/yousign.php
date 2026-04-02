c6fe7cd0fe518beeb279fccfccae50c6bcdcc723
Gabriele Pistoia <toshi.zl@gmail.com>
2026-04-02 12:03:21 +0200

    Removes deprecated branding ID configuration

--- config/yousign.php
+++ config/yousign.php
@@ -15,11 +15,6 @@ return [
      */
     'base_url' => env('YOUSIGN_BASE_URL', 'https://api-sandbox.yousign.app/v3'),
 
-    /*
-     * ID of the Branding to be used, found in your [Branding Dashboard](https://yousign.app/auth/settings/brandings)
-     */
-    'branding_id' => env('YOUSIGN_BRANDING_ID', ''),
-
     'webhooks' => [
         /*
          * Yousign will sign each webhook using a secret. You can find the used secret at the
